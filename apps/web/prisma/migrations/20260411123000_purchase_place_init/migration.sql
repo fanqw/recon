f08d920c02179db1d0e8792ca3c5fb00f61bcdc7
fanqw <fanqw.dev@gmail.com>
2026-04-11 14:04:52 +0800

    收敛Task2仅进货地范围

--- apps/web/prisma/migrations/20260411123000_purchase_place_init/migration.sql
+++ apps/web/prisma/migrations/20260411123000_purchase_place_init/migration.sql
@@ -11,11 +11,5 @@ CREATE TABLE "PurchasePlace" (
     CONSTRAINT "PurchasePlace_pkey" PRIMARY KEY ("id")
 );
 
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN "purchase_place_id" TEXT;
-
 -- CreateIndex
 CREATE UNIQUE INDEX "PurchasePlace_place_market_name_active_key" ON "PurchasePlace" ("place", "market_name") WHERE deleted = false;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_purchase_place_id_fkey" FOREIGN KEY ("purchase_place_id") REFERENCES "PurchasePlace"("id") ON DELETE SET NULL ON UPDATE CASCADE;
